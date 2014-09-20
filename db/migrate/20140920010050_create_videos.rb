9658f2b4b3394c4dcd7cb0bad99920dc25fe93c5
Alexandra Sos <SOSfaceKILLA@Macintosh-78.local>
2014-09-19 18:26:35 -0700

    first commit

---
+++ db/migrate/20140920010050_create_videos.rb
@@ -0,0 +1,15 @@
+class CreateVideos < ActiveRecord::Migration
+  def change
+    create_table :videos do |t|
+      t.string :link
+      t.string :title
+      t.string :uid
+      t.string :author
+      t.string :duration
+      t.integer :likes
+      t.integer :dislikes
+
+      t.timestamps
+    end
+  end
+end
